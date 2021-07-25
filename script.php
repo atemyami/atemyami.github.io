545f97ffbab70c68d559764d44df3acd2cc44d20
atemyami <78046028+atemyami@users.noreply.github.com>
2021-07-25 11:18:59 +0300

    Update script.php

--- script.php
+++ script.php
@@ -1,11 +1,26 @@
-<?php
-if($_POST){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+<?php 
+if (isset($_POST['sendinfoo'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
 
-    // Input sanitation omitted
-
-    // Send the email
-    mail("asilseeds@gmail.com", "Message from {$name} - {$email}", $message);
+  $msg = $_POST['addres'];
+  // Content-Type helps email client to parse file as HTML 
+  // therefore retaining styles
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $message = "<html>
+  <head>
+  	<title>New message from website contact form</title>
+  </head>
+  <body>
+  	<h1>" . $subject . "</h1>
+  	<p>".$msg."</p>
+  </body>
+  </html>";
+  if (mail('asilseeds@gmail.com', $subject, $message, $headers)) {
+   echo "Email sent";
+  }else{
+   echo "Failed to send email. Please try again later";
+  }
 }
+?>
