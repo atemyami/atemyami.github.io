af6a9d6f7e4a5346a13914a4e5cb9f11e2b06883
atemyami <78046028+atemyami@users.noreply.github.com>
2021-08-01 10:52:39 +0300

    Update train.js

--- train.js
+++ train.js
@@ -187,4 +187,4 @@ async function handleSubmit(event) {
         status.innerHTML = "Oops! There was a problem submitting your form"
     });
 }
-form.addEventListener("submit", handleSubmit) * /
+form.addEventListener("submit", handleSubmit) 
