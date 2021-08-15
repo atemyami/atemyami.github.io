59d0f04eac257b7a1e5bb3b695e9764b75953f3d
atemyami <78046028+atemyami@users.noreply.github.com>
2021-08-15 16:26:09 +0300

    Update train.php

--- train.php
+++ train.php
@@ -3,10 +3,9 @@
 include 'train.php';
 
 // The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
+$message = "Line";
 
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
+
 
 // Send
 mail('olaadases@gmail.com', 'My Subject', $message);
